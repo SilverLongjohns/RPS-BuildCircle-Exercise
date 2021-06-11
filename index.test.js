ad60a6a6d8a0bbe60eb49c197040c5e61758f975
JakeCummings <Jake.Cummings@rtgs.com>
2021-06-11 16:57:33 +0100

    supertesting

--- index.test.js
+++ index.test.js
@@ -6,9 +6,6 @@ const request = supertest(app);
 describe("When running the app", () => {
     it("Should display expected content on the root", async () => {
         const res = await request.get('/')
-
-        console.log(res.body)
-
-        expect(res.body).toEqual("Hello world")
-    })
-})
+        expect(res.text).toBe("Hello world")
+    });
+});
