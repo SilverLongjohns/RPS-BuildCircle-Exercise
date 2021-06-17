eed0e1e60149101cf370c3f24a57a55ed312f795
Gabriel Goiri <gabriel.goiri@rtgs.com>
2021-06-17 18:10:41 +0100

    result appears at server and console

--- game.js
+++ game.js
@@ -13,23 +13,37 @@ const startGame = () => {
 }
 
 const moveGame = (move) => {
+    let result
     const choices = ['ROCK', 'PAPER', 'SCISSORS'];
 
     const random = Math.floor(Math.random() * choices.length);
     console.log(random)
 
-    if(move === random) {
-        console.log('DRAW', move, random);
+    if(move === choices[random]) {
+        console.log('DRAW', move, choices[random]);
+        result = 'DRAW'
     }
 
-    if(
-        (move === 'PAPER' && random === 'ROCK') || 
-        (move === 'SCISSORS' && random === 'PAPER') || 
-        (move === 'ROCK' && random === 'SCISSORS')
+    else if(
+        (move === 'PAPER' && choices[random] === 'ROCK') || 
+        (move === 'SCISSORS' && choices[random] === 'PAPER') || 
+        (move === 'ROCK' && choices[random] === 'SCISSORS')
         ){
-        console.log('YOU WIN', move, random);
+        console.log('YOU WIN', move, choices[random]);
+        result = 'WIN'
+        gameInstance.score++
+
     } else {
-        console.log('YOU LOSE', move, random);
+        console.log('YOU LOSE', move, choices[random]);
+        result = 'LOSE'
+    }
+    return {
+        ...gameInstance,
+        lastGame: {
+            result,
+            yourMove: move,
+            computerMove: choices[random]
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const port = 5001;
 
 
 app.get('/', (req, res) => {
-    res.send("Hello world");
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/start', (req, res) => {
     res.send(game.startGame());
+})
+
+app.get('/start', (req, res) => {
+    res.redirect(302, "/game")
 });
 
 app.get('/score', (req, res) => {
@@ -18,20 +22,15 @@ app.get('/score', (req, res) => {
 });
 
 app.get('/game', (req, res) => {
+    res.sendFile(__dirname + '/game.html');
+    const gameId = res.send(gameInstance.id)
     res.send(id);
     res.send(score);
 });
 
-app.post(`/game?${id}&move=ROCK`, (req, res) => {
-    res.send(game.sendMove('ROCK'));
-});
-
-app.post(`/game?${game.gameInstance}&move=PAPER`, (req, res) => {
-    res.send(game.sendMove());
-});
-
-app.post(`/game?${game.gameInstance}&move=SCISSORS`, (req, res) => {
-    res.send(game.sendMove());
+app.post('/game', (req, res) => {
+    console.log('hello', req.body)
+    res.send(game.moveGame('ROCK'));
 });
 
 
