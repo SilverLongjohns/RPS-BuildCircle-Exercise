bbdcbd87513a590fb29147c82ae0d12187f676ea
Gabriel Goiri <gabriel.goiri@rtgs.com>
2021-06-17 16:14:32 +0100

    Saving id WIP

--- game.js
+++ game.js
@@ -1,13 +1,40 @@
 const uuid = require('uuid')
 
+let gameInstance = {
+
+}
+
 const startGame = () => {
-    return {
+    gameInstance = {
         id: uuid.v4(),
         score: 0,
+    }
+    return gameInstance
+}
+
+const moveGame = (move) => {
+    const choices = ['ROCK', 'PAPER', 'SCISSORS'];
+
+    const random = Math.floor(Math.random() * choices.length);
+    console.log(random)
+
+    if(move === random) {
+        console.log('DRAW', move, random);
+    }
+
+    if(
+        (move === 'PAPER' && random === 'ROCK') || 
+        (move === 'SCISSORS' && random === 'PAPER') || 
+        (move === 'ROCK' && random === 'SCISSORS')
+        ){
+        console.log('YOU WIN', move, random);
+    } else {
+        console.log('YOU LOSE', move, random);
     }
 }
 
 module.exports = {
     startGame,
-    id: uuid.v4(),
+    gameInstance,
+    moveGame,
 }
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { gameInstance } = require('./game');
 const game = require("./game");
 const app = express();
 const port = 5001;
@@ -15,6 +16,24 @@ app.post('/start', (req, res) => {
 app.get('/score', (req, res) => {
 
 });
+
+app.get('/game', (req, res) => {
+    res.send(id);
+    res.send(score);
+});
+
+app.post(`/game?${id}&move=ROCK`, (req, res) => {
+    res.send(game.sendMove('ROCK'));
+});
+
+app.post(`/game?${game.gameInstance}&move=PAPER`, (req, res) => {
+    res.send(game.sendMove());
+});
+
+app.post(`/game?${game.gameInstance}&move=SCISSORS`, (req, res) => {
+    res.send(game.sendMove());
+});
+
 
 app.listen(port);
 
